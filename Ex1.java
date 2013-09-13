e10d2d405f0e0c6218d72ed0e982fc6593c62010
Ohemeng Tutu Patrick <rasfarcorn@gmail.com>
2013-09-13 11:11:06 +0100

    My first commitment

--- Ex1.java
+++ Ex1.java
@@ -1,7 +1,7 @@
 public class Ex1{
 	public static void main(String[] args) {
 		
-		int i = 0;
+		int i = 200;
 		System.out.println(i);
 	}
 }
--- Ex2.java
+++ Ex2.java
@@ -1,7 +1,7 @@
 public class Ex2{
 	public static void main(String[] args) {
 		
-		int i=9;
+		int i=300;
 		System.out.println(i);
 	}
 }
